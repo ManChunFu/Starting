8b58ecadba164857f52072e8d7fae1eda928cb59
Mandy_Fu <man.chunfu@futuregames.nu>
2019-08-14 16:04:14 +0200

    First comment Added left flipper start playerInput script

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Camera _playerCamera;
+
+    private void Awake()
+    {
+        _playerCamera = Camera.main; //Camera.main uses find objects of tag internally, super rude.
+    }
+
+
+
+}

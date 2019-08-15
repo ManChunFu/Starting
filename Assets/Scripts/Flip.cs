8c139438fe504f08d616c8841f3b3362afe477dd
Mandy_Fu <man.chunfu@futuregames.nu>
2019-08-15 16:13:59 +0200

    flipper scripts playerInput scripts editor prefab brush script

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Flip : MonoBehaviour
+{
+    public float flipperSpeed = 1000f;
+    public float reverseMod = 1f;
+
+    [System.NonSerialized]
+    public bool isPressed = false;
+
+    private HingeJoint2D hinge;
+
+    private void Awake()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+        Assert.IsNotNull(hinge, "Couldn't find the hinge component."); //This will only run in debug mode
+    }
+
+    private void FixedUpdate()
+    {
+        if (isPressed)
+        {
+            JointMotor2D _motor = hinge.motor;
+            _motor.motorSpeed = reverseMod * flipperSpeed;
+            hinge.motor = _motor;
+        }
+        else
+        {
+            JointMotor2D _motor = hinge.motor;
+            _motor.motorSpeed = reverseMod * -flipperSpeed;
+            hinge.motor = _motor;
+        }
+
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,14 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerInput : MonoBehaviour
 {
-    private Camera _playerCamera;
+    private Camera playerCamera;
+    private Flip leftFlipper;
+    private Flip rightFlipper;
 
+    private const string leftflipperName = "LeftFlipper";
+    private const string rightFlipperName = "RightFlipper";
+
+    private Flip flipStript;
+
+    #region Unity methods
     private void Awake()
     {
-        _playerCamera = Camera.main; //Camera.main uses find objects of tag internally, super rude.
+        playerCamera = Camera.main; //Camera.main uses find objects of tag internally, super rude.
+
+        leftFlipper = GetFlipper(leftflipperName);
+        Assert.IsNotNull(leftFlipper, $"Child: {leftflipperName} was not found.");
+
+        rightFlipper = GetFlipper(rightFlipperName);
+        Assert.IsNotNull(rightFlipper, $"Child: {rightFlipperName} was not found.");
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
 
+    private void OnDestroy()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+
+    private void Update()
+    {
+        float xPosition = playerCamera.ScreenToWorldPoint(Input.mousePosition).x;
+        transform.position = new Vector3(xPosition, transform.position.y, transform.position.z);
+        leftFlipper.isPressed = Input.GetButton(leftflipperName);
+        rightFlipper.isPressed = Input.GetButton(rightFlipperName);
+        
+    }
+    #endregion Unity methods
+
+    private Flip GetFlipper(string flipperName)
+    {
+        return transform.Find(flipperName)?.GetComponent<Flip>();
+    }
 
 
 }
